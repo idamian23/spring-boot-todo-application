77b1caae6a476a4076bc3eb3b821750b72ec97b0
idamian23 <damian.ionut23@yahoo.ro>
2022-08-04 21:43:41 +0300

    description not empty

--- src/main/java/com/example/demo/models/TodoItem.java
+++ src/main/java/com/example/demo/models/TodoItem.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -24,6 +25,7 @@ public class TodoItem {
 
     @Getter
     @Setter
+    @NotBlank(message = "Description is required!")
     private String description;
 
     @Getter
